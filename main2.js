b4c83a194aeed73fd3cf9bdbf1b484067b95ec8b
Jorgee1 <jorge.ortega.p.1@gmail.com>
2020-12-31 23:51:59 -0500

    Main classes almost done, the game loop will be continued on another branch

--- main2.js
+++ main2.js
@@ -1,5 +1,7 @@
-class Rect{
-    constructor(x, y, h, w){
+class Rect
+{
+    constructor(x, y, h, w)
+    {
         this.x = x;
         this.y = y;
         this.h = h;
@@ -7,7 +9,8 @@ class Rect{
     }
 }
 
-class Point{
+class Point
+{
     constructor(x=0, y=0)
     {
         this.x = x;
@@ -17,7 +20,8 @@ class Point{
 
 
 
-class Screen{
+class Screen
+{
     constructor(id, color)
     {
         this.canvas = document.getElementById(id);
@@ -34,7 +38,55 @@ class Screen{
     }
 }
 
-class Food{
+
+class Input
+{
+    constructor()
+    {
+        this.left  = 65;
+        this.right = 68;
+        this.up    = 87;
+        this.down  = 83;
+
+        this.on  = true;
+        this.off = false;
+
+        this.keys = {}
+
+        this.keys[this.left]  = this.off;
+        this.keys[this.right] = this.off;
+        this.keys[this.up]    = this.off;
+        this.keys[this.down]  = this.off;
+
+        document.onkeydown = this.key_on.bind(this);
+        document.onkeyup   = this.key_off.bind(this);
+    }
+
+    key_on(event)
+    {
+        var key = event.keyCode;
+        var keys = Object.keys(this.keys)
+
+        if (keys.includes(key.toString()))
+        {
+            this.keys[key] = this.on;
+        }
+    }
+
+    key_off(event)
+    {
+        var key = event.keyCode;
+        var keys = Object.keys(this.keys)
+
+        if (keys.includes(key.toString()))
+        {
+            this.keys[key] = this.off;
+        }
+    }
+}
+
+class Food
+{
     constructor(x, y, h, w, color)
     {
         this.position = new Rect(x, y, h, w);
@@ -47,8 +99,58 @@ class Food{
         this.position.y = Math.random() * (screen_h - this.h);
     }
 
-    draw(context){
+    draw(context)
+    {
         context.fillStyle = this.color;
         context.fillRect(this.x, this.y, this.h, this.w);
+    }
+}
+
+class Snake
+{
+    constructor(x, y, h, w, speed, tail_size, color)
+    {
+        this.position = new Rect(x, y, h, w);
+        this.speed = new Point();
+        this.max_speed = new Point(speed, speed);
+        this.tail_size = tail_size;
+        this.tail = [new Rect(x, y, h, w)];
+        this.color = color;
+
+        this.direction = 0;
+    }
+
+    update()
+    {
+        this.position.x += this.speed.x;
+        this.position.y += this.speed.y;
+
+        for (var i = 0; i < this.tail_size - 1; i++)
+        {
+            this.tail[i] = this.tail[i+1];
+        }
+        this.tail[this.tail_size - 1] = this.position;
+
+        this.speed.x = 0;
+        this.speed.y = 0;
+    }
+
+    grow()
+    {
+        this.tail_size += 1;
+    }
+
+    draw()
+    {
+        context.fillStyle = this.color;
+        for (var i = 0; i < this.tail; i++)
+        {
+            context.fillRect(
+                this.tail[i].x,
+                this.tail[i].y,
+                this.tail[i].h,
+                this.tail[i].w
+            );
+        }
     }
 }
